514d44e3c29e7a30364477e9fe2fb5d4480e2ed0
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-09-22 18:35:22 +0200

    ejercicio 13 hecho

--- UD1/13_multiplications/main.js
+++ UD1/13_multiplications/main.js
@@ -1,5 +1,13 @@
 function printMultiplicationTables() {
 
+    for (let i = 1; i <= 10; i++) {
+        let result = " ";
+        for (let j = 1; j <= 10; j++) {
+            result += i * j + " ";
+        }
+        console.log(result)
+    }
+
 }
 
 printMultiplicationTables()
