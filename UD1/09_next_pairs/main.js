1abe986b86475a13d1144889a61b997063ac87fe
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-09-22 17:05:55 +0200

    realizado 08

--- UD1/09_next_pairs/main.js
+++ UD1/09_next_pairs/main.js
@@ -1,7 +1,18 @@
 const assert = require('assert').strict;
 
 function nextPairs(value) {
+    let result = []
+
+    if (value % 2 === 1){
+        result = [value-1, value+1]
+    } else {
+        result = [value-2, value+2]
+    }
+
+    return result
 }
 
-assert.deepStrictEqual(nextPairs(3), [2,4])
-assert.deepStrictEqual(nextPairs(4), [2,6])
+assert.deepStrictEqual(nextPairs(3), [2, 4])
+assert.deepStrictEqual(nextPairs(4), [2, 6])
+
+console.log("All tests passed!");
