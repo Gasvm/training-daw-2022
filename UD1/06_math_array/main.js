841e91513b34ad33197082b9ef707fbcac0cc2cc
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-09-22 16:30:59 +0200

    Realizacion ejercicio array 06

--- UD1/06_math_array/main.js
+++ UD1/06_math_array/main.js
@@ -8,11 +8,28 @@
 
 function doCalculation(array) {
 
+    //Creo una variable externa 
+    let suma = 0;
+    let max = array[0]
 
+    //Cada vez que paso un valor del array voy sumando el valor
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i]);
+        suma += array[i]
+    //si el valor base es menor que el comparado se guarda como el nuevo
+        if (array[i] > max) {
+            max = array[i]
+        }
+    }
+
+    console.log(`Suma del array ${suma}`);
+    console.log(`Promedio del array ${suma / array.length}`);
+    console.log(`Máximo del array ${max}`);
+    
 }
 
 
 
-doCalculation([1,2,3,4])
-doCalculation([5,5,5,5])
-doCalculation([1,1,1,2,2,2,3,3,3,4,4,4,5,5,5])
+doCalculation([1, 2, 3, 4])
+doCalculation([5, 5, 5, 5])
+doCalculation([1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5])
